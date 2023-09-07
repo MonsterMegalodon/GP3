ee92ad98015f5ad64a5fd8d3fdd86566d772d4b8
MonsterMegalodon <megalodonthemonster@outlook.com>
2023-09-07 15:43:52 -0500

    Fruit Juice

--- Assets/_MyFolder/Scripts/Fruit.cs
+++ Assets/_MyFolder/Scripts/Fruit.cs
@@ -11,11 +11,13 @@ public class Fruit : MonoBehaviour
 
     private Rigidbody fruitRigidbody;
     private Collider fruitCollider;
+    private ParticleSystem juiceParticalEffect;
 
     private void Awake()
     {
         fruitRigidbody = GetComponent<Rigidbody>();
         fruitCollider = GetComponent<Collider>();
+        juiceParticalEffect = GetComponentInChildren<ParticleSystem>();
     }
     private void Sliced(Vector3 direction, Vector3 position, float force)
     {
@@ -23,6 +25,7 @@ public class Fruit : MonoBehaviour
         sliced.SetActive(true);
 
         fruitCollider.enabled = false;
+        juiceParticalEffect.Play();
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;        // rotate the fruit to match the cut
         sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);        //match fruit to the angle that its being cut at
